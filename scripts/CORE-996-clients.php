382b9f2ed8ced8f155c89897a7f8b30f1ffa50f5
Daryle Dakis <daryle@codeninja.co>
2017-04-26 13:25:48 -0700

    Add new script files

---
+++ scripts/CORE-996-clients.php
@@ -0,0 +1,60 @@
+<?php
+    date_default_timezone_set('America/Los_Angeles');
+    set_time_limit(0);
+    ob_start();
+    /* 
+     * To change this license header, choose License Headers in Project Properties.
+     * To change this template file, choose Tools | Templates
+     * and open the template in the editor.
+     */
+
+    $host = "sql-core-web-write.importgenius.private.com";
+//    $host = "localhost";
+    $user = "core";
+    $pass = "ajnin_edoc_12";
+
+    $database = "core";
+
+    $nbsp = PHP_EOL;
+//    $nbsp = "<br />";
+
+    $konek = mysql_connect($host, $user, $pass) or die("Unable to connect to {$host}: ".mysql_error());
+    $db = mysql_select_db($database);
+
+    $sql = "SELECT id, username, email, phone, phone_ext, alt_phone, alt_phone_ext, alt_phone_2, alt_phone_2_ext FROM users
+            WHERE 
+            deleted = 0 AND email NOT LIKE '%echosigntest%' AND email NOT LIKE '%devtest%' AND email NOT LIKE '%importgenius%' AND email NOT LIKE '%codeninja%' AND email != 'ryang600@yahoo.com';
+            ;";
+    echo $sql.$nbsp.$nbsp;
+    $que = mysql_query($sql, $konek) or die("SQL: ".mysql_error());
+    $num = mysql_num_rows($que);
+
+    $affected = 0;
+    if ($num > 0) {
+        while ($row = mysql_fetch_assoc($que)) {
+            $id = $row['id'];
+            $phones = [$row['phone'], $row['alt_phone'], $row['alt_phone_2']];
+            list ($phone, $alt_phone, $alt_phone_2) = sanitizePhone($phones);
+
+            $updatesql = "UPDATE users SET phone = '{$phone}', alt_phone = '{$alt_phone}', alt_phone_2 = '{$alt_phone_2}' WHERE id = {$id}";
+//            echo $updatesql.$nbsp.$nbsp;
+            $updateque = mysql_query($updatesql, $konek) or die("Updatesql: ".mysql_error());
+            $affected += mysql_affected_rows();
+        }
+    }
+
+    function sanitizePhone($phone) {
+        if (!is_array($phone)) {
+            return preg_replace("/[^A-Za-z0-9,*\/]/", "", $phone);
+        }
+
+        $sanitizedPhone = [];
+        foreach ($phone AS $val) {
+            $sanitizedPhone[] = preg_replace("/[^A-Za-z0-9,*\/]/", "", $val);
+        }
+
+        return $sanitizedPhone;
+    }
+
+    echo "Affected: {$affected}".$nbsp.$nbsp;
+    ob_flush();
